1a0ce37489a53b1cf0f3a1a053035e13c4d3a418
NishaantSoni <134582582+NishaantSoni@users.noreply.github.com>
2023-06-07 10:58:35 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 #import snowflake.connector
 from functools import wraps
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.express as px
 import time
 
